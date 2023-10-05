1738a7d271865dd5cfd45a2dde179fe4eafd8a0c
AugustoQuispe1 <augustoq93@gmail.com>
2023-10-04 22:00:49 -0300

    Add project files.

---
+++ DynamicMemorySortedList/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+	{
+	static void Main()
+		{
+		SortedList<string, string> diccionario = new SortedList<string, string>();
+
+		// Agregar palabras al diccionario
+		diccionario.Add("hello", "hola");
+		diccionario.Add("goodbye", "adiós");
+		diccionario.Add("bicycle", "bicicleta");
+		diccionario.Add("car", "coche");
+
+	
+		string palabraBuscada = "hello";
+		if (diccionario.ContainsKey(palabraBuscada))
+			{
+			Console.WriteLine($"{palabraBuscada} en español es: {diccionario[palabraBuscada]}");
+			}
+		else
+			{
+			Console.WriteLine($"La palabra '{palabraBuscada}' no está en el diccionario.");
+			}
+		}
+	}
